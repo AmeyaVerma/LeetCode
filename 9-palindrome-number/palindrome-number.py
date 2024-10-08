0fc5c3afc2051fb87c16b9052f59d65386dd8e17
Ameya Verma <103594502+AmeyaVerma@users.noreply.github.com>
2024-10-08 23:40:43 +0530

    Time: 57 ms (41.88%) | Memory: 16.4 MB (87.48%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,5 +1,11 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        a=str(x)
-        return a==a[::-1]
-        
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False
+        reverse_x= str(x)
+        reverse_x=reverse_x[::-1]
+        reverse_x=int(reverse_x)
+        if reverse_x== x:
+            return True
+        else:
+            return False
