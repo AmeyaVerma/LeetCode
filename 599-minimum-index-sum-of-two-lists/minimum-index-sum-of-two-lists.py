1e5f2d553df113949db837f14bcabdceaa55910e
Ameya Verma <103594502+AmeyaVerma@users.noreply.github.com>
2024-11-10 02:12:23 -0800

    Time: 105 ms (24.09%) | Memory: 17.1 MB (66.85%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
@@ -1,24 +1,28 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
-        mini = float('inf')
-        res= []
-        for idx2, i2 in enumerate(list1):
-            for idx1, i1 in enumerate(list2):
-                if i1==i2 and idx1+idx2<= mini:
-                    if res is None or idx1+idx2==mini:
-                        print("Hitting")
-                        res.append(i2)
-                    # elif res is not None and idx1+idx2<mini:
-                    else:
-                        res=[]
-                        res.append(i2)
-                    
-                    mini= idx2+idx1
-                    break
+        same = [word for word in list1 if word in list2]
+        same.sort()
+        dic={}
+        for word in same:
+            idx1=list1.index(word)
+            idx2=list2.index(word)
+            dic[word]=idx1+idx2
+        
+        dic = dict(sorted(dic.items(), key=lambda item: item[1]))
+        val=list(dic.values())
+        val.sort()
+        small=val[0]
+        res=[]
+
+        for word in dic:
+            if dic[word]==small:
+                res.append(word)
         return res
-        # for idx_list2,i in enumerate(list2):
-        #     if i in list1:
-        #         idx_of_element = [index for index, value in enumerate(list1) if value == i]
-        #         for idx_list1 in range(len(idx_of_element)):
-        #             mini= min(idx_list1-idx_list2)
-        # return mini if mini>0 else -mini
+
+
+
+        
+            
+
+        
+        
