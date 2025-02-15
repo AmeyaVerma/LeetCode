d190f1e99e4e75494ce362af7c524f5f642b1290
Ameya Verma <103594502+AmeyaVerma@users.noreply.github.com>
2025-02-15 17:42:53 +0530

    Time: 17 ms (42.87%) | Memory: 22.8 MB (12.92%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,11 +1,16 @@
 from collections import defaultdict
+
 class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        res=defaultdict(list)
+    def groupAnagrams(self, strs):
+        res = defaultdict(list)  # Dictionary with lists as default values
 
         for s in strs:
-            count=[0]*26
+            count = [0] * 26  # Array to store letter counts
+            
             for c in s:
-                count[ord(c)-ord("a")]+=1
-            res[tuple(count)].append(s)
-        return res.values()
+                count[ord(c) - ord('a')] += 1  # Count occurrences of each letter
+            
+            res[tuple(count)].append(s)  # Convert count to tuple and use as key
+
+        return list(res.values())  # Convert to list and return
+
